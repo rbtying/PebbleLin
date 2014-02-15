d28790ea09ad8c000038ff47ff1bfa9fe44b6644
Robert Ying <rbtying@aeturnalus.com>
2014-02-15 17:15:01 -0500

    Added StartActivity

---
+++ src/com/lynbrook/pebblelin/StartActivity.java
@@ -0,0 +1,52 @@
+package com.lynbrook.pebblelin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.lynbrook.pebblin.R;
+
+public class StartActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTitle(R.string.app_name);
+    setContentView(R.layout.activity_main);
+
+    Button b = (Button) findViewById(R.id.huebutton);
+    b.setOnClickListener(new OnClickListener() {
+      public void onClick(View v) {
+        gotohuething();
+
+      }
+    });
+
+    Button b2 = (Button) findViewById(R.id.realbutton);
+    b2.setOnClickListener(new OnClickListener() {
+      public void onClick(View v) {
+        gotorealthing();
+      }
+    });
+  }
+
+  public void gotohuething() {
+    Intent intent = new Intent(getApplicationContext(), PHHomeActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) intent.addFlags(0x8000); // equal to
+    startActivity(intent);
+  }
+
+  public void gotorealthing() {
+    Intent intent = new Intent(getApplicationContext(), PebblinActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) intent.addFlags(0x8000); // equal to
+    startActivity(intent);
+  }
+
+}
