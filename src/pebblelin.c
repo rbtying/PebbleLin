3fbd18be946049a60ae46f7d6cb6a357ba6e9b56
Robert Ying <rbtying@aeturnalus.com>
2014-03-20 12:13:21 -0400

    Added inbox_received_callback and comments

--- src/pebblelin.c
+++ src/pebblelin.c
@@ -1,19 +1,38 @@
 #include <pebble.h>
 
+static const uint32_t INBOX_BUFFER_SIZE = 64;
+static const uint32_t OUTBOX_BUFFER_SIZE = 128;
+
+/*!
+ * Graphics elements
+ */
 static Window *window;
 static TextLayer *text_layer_1;
 
+/*!
+ * Used to store the gravity vector, acceleration vector, and velocity vector
+ */
 static int g[3];
 static int a[3];
 static int v[3];
 
+/*!
+ * Integrates acceleration to get velocity on timer
+ */
 static AppTimer *timer;
 
-void send_msg() {
+/*!
+ * Sends the accelerometer and velocity data to the phone
+ *
+ * \return Result of outbox success
+ */
+AppMessageResult send_msg() {
     DictionaryIterator *iter;
 
-    if (app_message_outbox_begin(&iter) != APP_MSG_OK) {
-        return;
+    AppMessageResult ret;
+
+    if ((ret = app_message_outbox_begin(&iter)) != APP_MSG_OK) {
+        return ret;
     }
 
     dict_write_int16(iter, 0, v[0]);
@@ -26,19 +45,51 @@ void send_msg() {
     dict_write_int16(iter, 7, g[1]);
     dict_write_int16(iter, 8, g[2]);
 
-    app_message_outbox_send();
+    return app_message_outbox_send();
 }
 
+/*!
+ * Send the next message once an ack is received, so that phone is kept as up
+ * to date as possible.
+ *
+ * \param iterator ignored
+ * \param reason ignored
+ * \param context ignored
+ */
 void outbox_fail_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) 
 {
-   send_msg(); 
+    send_msg(); 
 }
 
+/*!
+ * Send the next message once an ack is received, so that phone is kept as up
+ * to date as possible.
+ *
+ * \param iterator ignored
+ * \param context ignored
+ */
 void outbox_success_callback(DictionaryIterator *iterator, void *context)
 {
     send_msg();
 }
 
+/*!
+ * Processes the message received from the phone
+ *
+ * \param iterator the dictionary sent to the pebble
+ * \param context unused
+ */
+void inbox_received_callback(DictionaryIterator *iterator, void *context)
+{
+}
+
+/*!
+ * Callback function for the accelerometer service, calculates the
+ * low-pass-filtered and integrated data results
+ *
+ * \param data the accelerometer data
+ * \param num_samples the number of samples in the dataset, should be 1
+ */
 void accel_handler(AccelData *data, uint32_t num_samples)
 {
     // data is an array of num_samples elements.
@@ -70,6 +121,11 @@ void accel_handler(AccelData *data, uint32_t num_samples)
     text_layer_set_text(text_layer_1, buf);
 }
 
+/*!
+ * Initializes graphic elements and subscribes to accelerometer
+ * 
+ * \param window the app's main window
+ */
 void window_load(Window *window)
 {
     Layer *window_layer = window_get_root_layer(window);
@@ -81,6 +137,12 @@ void window_load(Window *window)
     accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
 }
 
+/*!
+ * Destroys elements when app goes out of scope, and unsubscribes from
+ * the accelerometer.
+ *
+ * \param window the app's main window
+ */
 void window_unload(Window *window)
 {
     // Call this before destroying text_layer, because it can change the text
@@ -89,6 +151,9 @@ void window_unload(Window *window)
     text_layer_destroy(text_layer_1);
 }
 
+/*!
+ * Asks for data from the accelerometer, and schedules itself for the future
+ */
 void timer_callback() {
     AccelData accel = {0, 0, 0, 0, 0};
     accel_service_peek(&accel);
@@ -96,6 +161,11 @@ void timer_callback() {
     timer = app_timer_register(20, timer_callback, NULL);
 }
 
+/*!
+ * Initializes the AppMessage communications, and registers callbacks.
+ * 
+ * Starts the send_msg() loop
+ */
 static void app_message_init(void) {
     // Reduce the sniff interval for more responsive messaging at the expense of
     // increased energy consumption by the Bluetooth module
@@ -106,12 +176,18 @@ static void app_message_init(void) {
     timer = app_timer_register(20, timer_callback, NULL);
     app_message_register_outbox_failed(outbox_fail_callback);
     app_message_register_outbox_sent(outbox_success_callback);
+    app_message_register_inbox_received(inbox_received_callback);
     
     // Init buffers
-    app_message_open(64, 16 * 9);
+    app_message_open(INBOX_BUFFER_SIZE, OUTBOX_BUFFER_SIZE);
+
+    // kick off send_msg
     send_msg();
 }
 
+/*!
+ * Main function 
+ */
 int main()
 {
     window = window_create();
